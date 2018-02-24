282f73ac93a1e45373a59dff3c4a9d08688efaaf
Jardine <jardine.miller@gmail.com>
2018-02-24 19:10:34 +0000

    DB now populating from CSV file

--- quiz_api/app/models/quiz.rb
+++ quiz_api/app/models/quiz.rb
@@ -1,5 +1,6 @@
 class Quiz < ApplicationRecord
   has_many :questions
   has_many :results
-  has_many :users, through :results
+  has_many :users, through: :results
 end
+ 
--- quiz_api/db/seeds.rb
+++ quiz_api/db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_question(row, quiz)
+  Question.create({
+    text: row['Question'],
+    quiz_id: quiz.id
+    })
+end
+
+def create_answer(row, index, question)
+  Answer.create({
+    text: row[index],
+    points: row.headers[index].to_i,
+    question_id: question.id
+    })
+end
+
+quiz = Quiz.create({
+  name: "Float Quiz"
+  })
+
+CSV.foreach(Rails.root.join('db', 'data', 'quiz.csv'), :headers => true) do |row|
+  question = create_question(row, quiz)
+  num_of_columns = row.headers.length - 1
+  for index in 1..num_of_columns do
+    create_answer(row, index, question)
+  end
+end
