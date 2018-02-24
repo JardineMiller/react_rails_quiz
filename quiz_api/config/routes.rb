09a1ddedd9d8e3b89799857c815dd0dd2a4979f8
Jardine <jardine.miller@gmail.com>
2018-02-24 20:53:47 +0000

    readjusted routes to reflect controller changes

--- quiz_api/config/routes.rb
+++ quiz_api/config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :quizzes
-      resources :questions do
-        resources :answers
-      end
-      resources :answers
     end
   end
 end
